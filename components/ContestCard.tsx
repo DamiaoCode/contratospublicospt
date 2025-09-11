37cf3de9e5a38a86cea8580c03847784920fef11
DamiaoCode <damiaocode@gmail.com>
2025-09-11 09:15:27 +0100

    Remover limite de caracteres no titulo do card extendido

--- components/ContestCard.tsx
+++ components/ContestCard.tsx
@@ -150,10 +150,7 @@ export default function ContestCard({
           <div className="space-y-3 text-sm">
             {/* Informações básicas */}
             <div>
-              <span className="font-medium text-gray-600">Título:</span>{' '}
-              {concurso.titulo && concurso.titulo.length >= 60
-                ? `${concurso.titulo.slice(0, 57)}(...)`
-                : concurso.titulo}
+              <span className="font-medium text-gray-600">Título:</span> {concurso.titulo}
             </div>
             <div>
               <span className="font-medium text-gray-600">Entidade:</span> {concurso.entidade}
